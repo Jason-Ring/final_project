56173ede42fd986124cef9e1f76002592d3e138f
Jason Ring <jason.ring@kellogg.northwestern.edu>
2021-03-13 17:09:31 -0600

    3/13 - 5pm

--- netlify/functions/firebase.js
+++ netlify/functions/firebase.js
@@ -1,7 +1,15 @@
 const firebase = require("firebase/app")
 require("firebase/firestore")
 
-const firebaseConfig = {} // replace
+const firebaseConfig = {
+  apiKey: "AIzaSyC2lQ0bHcGNs2vaioAGpO-j8OzDWrDox9I",
+  authDomain: "kiei451-c5332.firebaseapp.com",
+  projectId: "kiei451-c5332",
+  storageBucket: "kiei451-c5332.appspot.com",
+  messagingSenderId: "60157349058",
+  appId: "1:60157349058:web:13a7dd49bea7aa73adff5b",
+  measurementId: "G-76T99EBXYQ"
+} // replace
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig)
