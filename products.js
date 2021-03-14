2e451d3809c1aa35c98a3fe298f9fbefea4185aa
Jason Ring <jason.ring@kellogg.northwestern.edu>
2021-03-14 11:10:01 -0500

    initial

---
+++ products.js
@@ -0,0 +1,107 @@
+let products = [
+    {
+        name: "TKO Tri-Grip® Hex Urethane Dumbbells",
+        id: "D1",
+        category: "Dumbells",
+        image: `https://smhttp-ssl-18062.nexcesscdn.net/media/prod.thumb/t/k/tko-tri-grip-hex-dumbbells-urethane.jpg`,
+        price: 29.99,
+        description: `The TKO Tri-Grip® Hex Dumbbells provide high-quality with a stylish design to enhance the look of any facility. Built from the highest-end material and guaranteed to last in the highest used facilities like health clubs, college rec and high school levels.  They are designed with a proprietary handle to fit the natural arch of the hand, which increases safety and effectiveness.  They provide an exceptional grip for all users.`
+    },
+    {
+        name: "Hampton Dura-Prostyle Dumbbell",
+        id: "D2",
+        category: "Dumbells",
+        image: `https://smhttp-ssl-18062.nexcesscdn.net/media/prod.thumb/h/a/hampton-dumbbell-dpu_10_19.jpg`,
+        price: 29.99,
+        description: `The Hampton Dura-Prostyle Dumbbell combines everything you want in a Dumbbell into a single, beautifully-designed unit. It’s practical and efficient, offering you the choice of vertical or horizontal storage based on your gym’s configuration and space issues. It’s ergonomically-designed, with a unique 35-mm handle contoured to fit the natural arch of the hand for increased safety and effectiveness, with a coarse knurling for exceptional grip.`
+    },
+    {
+        name: "Adjustable Dumbbell Set",
+        id: "D3",
+        category: "Dumbells",
+        image: `https://smhttp-ssl-18062.nexcesscdn.net/media/prod.thumb/c/o/core-adjustable-dumbbell-set-01.jpg`,
+        price: 99.99,
+        description: `Core Home Fitness's Adjustable Dumbbells let you change weights in 5 lb increments with the simple twist of a handle – no knobs to turn or levers to slide. With weights ranging from 5 to 50 lbs, our dumbbell gives you an entire 10-piece set in a single dumbbell – all with a durable construction and a modern, lightweight aluminum cradle. It’s a great way to get a real workout with real results – all from a smart, compact design.`
+    },
+    {
+        name: "Profile® Squat Rack with Kipping Bar™",
+        id: "P1",
+        category: "Power Racks",
+        image: `https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1599145859-profile-rack-jpg-1.jpg?crop=1xw:1.00xh;center,top&resize=768:*`,
+        price: 799.99,
+        description: `Rogue is one of the top rack brands out there, and this 90” squat stand is a great way to score their characteristic quality and function (although you will have to buy a bench, barbell and weight plate set separately). At 49” W x 48” D x 98” H, this rack is pretty compact, so you can easily fit it in your garage. Choose between a fully-integrated fat/skinny pull-up bar or a single 1.25” pull-up bar.`
+    },
+    {
+        name: "SML-2 Rogue 90",
+        id: "P2",
+        category: "Power Racks",
+        image: `https://smhttp-ssl-18062.nexcesscdn.net/media/prod.thumb/t/k/tko-tri-grip-hex-dumbbells-urethane.jpg`,
+        price: 29.99,
+        description: `The TKO Tri-Grip® Hex Dumbbells provide high-quality with a stylish design to enhance the look of any facility. Built from the highest-end material and guaranteed to last in the highest used facilities like health clubs, college rec and high school levels.  They are designed with a proprietary handle to fit the natural arch of the hand, which increases safety and effectiveness.  They provide an exceptional grip for all users.`
+    },
+    {
+        name: "TKO Tri-Grip® Hex Urethane Dumbbells",
+        id: "1",
+        category: "Dumbells",
+        image: `https://smhttp-ssl-18062.nexcesscdn.net/media/prod.thumb/t/k/tko-tri-grip-hex-dumbbells-urethane.jpg`,
+        price: 29.99,
+        description: `The TKO Tri-Grip® Hex Dumbbells provide high-quality with a stylish design to enhance the look of any facility. Built from the highest-end material and guaranteed to last in the highest used facilities like health clubs, college rec and high school levels.  They are designed with a proprietary handle to fit the natural arch of the hand, which increases safety and effectiveness.  They provide an exceptional grip for all users.`
+    },
+    {
+        name: "TKO Tri-Grip® Hex Urethane Dumbbells",
+        id: "1",
+        category: "Dumbells",
+        image: `https://smhttp-ssl-18062.nexcesscdn.net/media/prod.thumb/t/k/tko-tri-grip-hex-dumbbells-urethane.jpg`,
+        price: 29.99,
+        description: `The TKO Tri-Grip® Hex Dumbbells provide high-quality with a stylish design to enhance the look of any facility. Built from the highest-end material and guaranteed to last in the highest used facilities like health clubs, college rec and high school levels.  They are designed with a proprietary handle to fit the natural arch of the hand, which increases safety and effectiveness.  They provide an exceptional grip for all users.`
+    },
+    {
+        name: "TKO Tri-Grip® Hex Urethane Dumbbells",
+        id: "1",
+        category: "Dumbells",
+        image: `https://smhttp-ssl-18062.nexcesscdn.net/media/prod.thumb/t/k/tko-tri-grip-hex-dumbbells-urethane.jpg`,
+        price: 29.99,
+        description: `The TKO Tri-Grip® Hex Dumbbells provide high-quality with a stylish design to enhance the look of any facility. Built from the highest-end material and guaranteed to last in the highest used facilities like health clubs, college rec and high school levels.  They are designed with a proprietary handle to fit the natural arch of the hand, which increases safety and effectiveness.  They provide an exceptional grip for all users.`
+    },
+    {
+        name: "TKO Tri-Grip® Hex Urethane Dumbbells",
+        id: "1",
+        category: "Dumbells",
+        image: `https://smhttp-ssl-18062.nexcesscdn.net/media/prod.thumb/t/k/tko-tri-grip-hex-dumbbells-urethane.jpg`,
+        price: 29.99,
+        description: `The TKO Tri-Grip® Hex Dumbbells provide high-quality with a stylish design to enhance the look of any facility. Built from the highest-end material and guaranteed to last in the highest used facilities like health clubs, college rec and high school levels.  They are designed with a proprietary handle to fit the natural arch of the hand, which increases safety and effectiveness.  They provide an exceptional grip for all users.`
+    },
+    {
+        name: "TKO Tri-Grip® Hex Urethane Dumbbells",
+        id: "1",
+        category: "Dumbells",
+        image: `https://smhttp-ssl-18062.nexcesscdn.net/media/prod.thumb/t/k/tko-tri-grip-hex-dumbbells-urethane.jpg`,
+        price: 29.99,
+        description: `The TKO Tri-Grip® Hex Dumbbells provide high-quality with a stylish design to enhance the look of any facility. Built from the highest-end material and guaranteed to last in the highest used facilities like health clubs, college rec and high school levels.  They are designed with a proprietary handle to fit the natural arch of the hand, which increases safety and effectiveness.  They provide an exceptional grip for all users.`
+    },
+    {
+        name: "TKO Tri-Grip® Hex Urethane Dumbbells",
+        {
+            name: "TKO Tri-Grip® Hex Urethane Dumbbells",
+            id: "1",
+            category: "Dumbells",
+            image: `https://smhttp-ssl-18062.nexcesscdn.net/media/prod.thumb/t/k/tko-tri-grip-hex-dumbbells-urethane.jpg`,
+            price: 29.99,
+            description: `The TKO Tri-Grip® Hex Dumbbells provide high-quality with a stylish design to enhance the look of any facility. Built from the highest-end material and guaranteed to last in the highest used facilities like health clubs, college rec and high school levels.  They are designed with a proprietary handle to fit the natural arch of the hand, which increases safety and effectiveness.  They provide an exceptional grip for all users.`
+        },
+        {
+            name: "TKO Tri-Grip® Hex Urethane Dumbbells",
+            id: "1",
+            category: "Dumbells",
+            image: `https://smhttp-ssl-18062.nexcesscdn.net/media/prod.thumb/t/k/tko-tri-grip-hex-dumbbells-urethane.jpg`,
+            price: 29.99,
+            description: `The TKO Tri-Grip® Hex Dumbbells provide high-quality with a stylish design to enhance the look of any facility. Built from the highest-end material and guaranteed to last in the highest used facilities like health clubs, college rec and high school levels.  They are designed with a proprietary handle to fit the natural arch of the hand, which increases safety and effectiveness.  They provide an exceptional grip for all users.`
+        },
+        id: "1",
+        category: "Dumbells",
+        image: `https://smhttp-ssl-18062.nexcesscdn.net/media/prod.thumb/t/k/tko-tri-grip-hex-dumbbells-urethane.jpg`,
+        price: 29.99,
+        description: `The TKO Tri-Grip® Hex Dumbbells provide high-quality with a stylish design to enhance the look of any facility. Built from the highest-end material and guaranteed to last in the highest used facilities like health clubs, college rec and high school levels.  They are designed with a proprietary handle to fit the natural arch of the hand, which increases safety and effectiveness.  They provide an exceptional grip for all users.`
+    },
+    
+]
